07b10e3288f32610506a85bdbfa9159f0e9a7275
aryan-thawkar <142441658+aryan-thawkar@users.noreply.github.com>
2024-02-10 11:46:17 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+#1 W.A.P to find factorial of number
+
+def factorial(n):
+    if n < 0:
+        return 0
+    elif n == 0 or n == 1:
+        return 1
+    else :
+        fact = 1
+        while (n > 1) :
+            fact *= n
+            n -= 1
+        return fact
+
+#driver code
+n = int(input("Enter a number : "))
+fact = factorial(n)
+print("The factorial of ",n," is ",fact)
